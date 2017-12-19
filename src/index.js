050d67c1df25c3f9bcf74875f4e24ad1c4dac18c
Brown0101 <dwayne@dontasweb.com>
2017-12-18 18:54:51 -0600

    Working on base application layout.

--- src/index.js
+++ src/index.js
@@ -4,5 +4,8 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+// My added imports
+import App from 'Components/App/App';
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
